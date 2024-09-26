c2fb5a1d753fe811ccb8a7afd495f9e0e957be69
RubénHdz06 <ruben.hernandez9106@gmail.com>
2024-09-26 14:56:51 -0600

    Add message list by user and modify request message body

--- real-time-notifications-microservice/src/main/java/com/example/notificationservice/controller/NotificationController.java
+++ real-time-notifications-microservice/src/main/java/com/example/notificationservice/controller/NotificationController.java
@@ -2,6 +2,7 @@ package com.example.notificationservice.controller;
 
 import com.example.notificationservice.HttpResponse.CustomApiResponse;
 import com.example.notificationservice.HttpResponse.ResponseUtil;
+import com.example.notificationservice.dto.NotificationRequest;
 import com.example.notificationservice.entity.Notification;
 import com.example.notificationservice.repository.NotificationRepository;
 import com.example.notificationservice.service.NotificationService;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,6 +22,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api-clients/v1.0/notifications")
@@ -68,6 +71,55 @@ public class NotificationController {
 
 
 
+    // Anotaciones para documentar el el edpoint de II-.  getNotificationsUserIdByMessage
+    @Operation(summary = "Count userReferenceIds by message", description = "Count the number of unique userReferenceIds for each message")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved notifications",
+                    content = @Content(schema = @Schema(implementation = Map.class))),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
+    // II. Verificar todos los userReferenceId que tiene el mismo mensaje o notificacions
+    @GetMapping("/users/notifications")
+    public Mono<ResponseEntity<CustomApiResponse<List<Notification>>>> getNotificationsUserIdforMessage(@RequestBody String message){
+        return notificationService.getNotificationsByMessage(message);
+    }
+
+
+    // Anotaciones para documentar el el edpoint de I-. createNotification
+    @Operation(summary = "Create a notification for a user", description = "Create a new notification for a specific user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Notification successfully created",
+                    content = @Content(schema = @Schema(implementation = Notification.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request data")
+    })
+    // I.Crear mensaje o notificacion al usuario
+    @PostMapping("/users/{userId}")  // I. Metodo para crear la notificacion o el mensaje del usuario
+    public Mono<ResponseEntity<CustomApiResponse<Notification>>> createNotification
+    (@PathVariable String userId, @Valid @RequestBody NotificationRequest notificationRequest) {
+        // 1.Creamos la  instancia de Notification  en el que setearemos el valor del mensaje
+        Notification notification = new Notification();
+        // 2. Recuperamos  solo el valor del campo "message de la clase notificationRequest"
+        notification.setMessage(notificationRequest.getMessage());
+        //3.  Llamamos al Servicio notificationService para crear la Notificación pasando el userId  y la instancia Notification
+        return notificationService.createNotification(userId, notification)
+                //4. con el ResponseUtil.createSuccessResponse => Nos indica si la creacion es exitosa y se envia la respuesta de mensaje en formato JSON
+                .flatMap(createdNotification -> ResponseUtil.createSuccessResponse("Notificación creada con éxito", createdNotification))
+                //5. ResponseUtil.createErrorResponse => Sí ocurre un error , envia el mensaje error con el status de respuesta http
+                .onErrorResume(e -> ResponseUtil.createErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST));
+    }
+
+    @PutMapping("/{notificationId}/read-and-delete")
+    @Operation(summary = "Mark a notification as read and delete it", description = "Marks a notification as read and then deletes it from both the notification collection and the user's notification list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Notification successfully marked as read and deleted"),
+            @ApiResponse(responseCode = "404", description = "Notification not found")
+    })
+    public Mono<ResponseEntity<CustomApiResponse<Void>>> markNotificationAsReadAndDelete(@PathVariable String notificationId) {
+        return notificationService.markNotificationAsReadAndDelete(notificationId)
+                .flatMap(successMessage -> ResponseUtil.createSuccessResponse(successMessage, (Void) null))
+                .onErrorResume(e -> ResponseUtil.createErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND));
+    }
+    /*
                     //Endpoint para obtener las notificaciones de un usuario especifico
                     @Operation(summary = "Get stream user notifications", description = "Stream notifications for a specific user in real time")
                     @ApiResponses(value = {
@@ -78,10 +130,9 @@ public class NotificationController {
                     @GetMapping(value = "/stream/{userId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
                     public Flux<Notification> streamNotifications(@PathVariable String userId) {
                         return notificationService.getNotificationsStream(userId);  // Flujo de notificaciones para un usuario específico
-                    }
+                    }  */
 
-
-
+/*
                             @Operation(summary = "Create a notification for a user", description = "Create a new notification for a specific user")
                             @ApiResponses(value = {
                                     @ApiResponse(responseCode = "201", description = "Notification successfully created",
@@ -93,23 +144,13 @@ public class NotificationController {
                                 return notificationService.createNotification(userId, notification)
                                         .flatMap(createdNotification -> ResponseUtil.createSuccessResponse("Notificación creada con éxito", createdNotification))
                                         .onErrorResume(e -> ResponseUtil.createErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST));
-                            }
+                            }  */
 
 
 
 
 
-                                    @PutMapping("/{notificationId}/read-and-delete")
-                                    @Operation(summary = "Mark a notification as read and delete it", description = "Marks a notification as read and then deletes it from both the notification collection and the user's notification list")
-                                    @ApiResponses(value = {
-                                            @ApiResponse(responseCode = "200", description = "Notification successfully marked as read and deleted"),
-                                            @ApiResponse(responseCode = "404", description = "Notification not found")
-                                    })
-                                    public Mono<ResponseEntity<CustomApiResponse<Void>>> markNotificationAsReadAndDelete(@PathVariable String notificationId) {
-                                        return notificationService.markNotificationAsReadAndDelete(notificationId)
-                                                .flatMap(successMessage -> ResponseUtil.createSuccessResponse(successMessage, (Void) null))
-                                                .onErrorResume(e -> ResponseUtil.createErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND));
-                                    }
+
 
 
 
--- real-time-notifications-microservice/src/main/java/com/example/notificationservice/service/NotificationService.java
+++ real-time-notifications-microservice/src/main/java/com/example/notificationservice/service/NotificationService.java
@@ -1,12 +1,16 @@
 package com.example.notificationservice.service;
 
 
+import com.example.notificationservice.HttpResponse.CustomApiResponse;
+import com.example.notificationservice.HttpResponse.ResponseUtil;
 import com.example.notificationservice.entity.Notification;
 import com.example.notificationservice.repository.NotificationRepository;
 import com.example.notificationservice.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -17,6 +21,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -100,74 +105,96 @@ public class NotificationService {
     }
 
 
-
-            // Flujo de notificaciones para un usuario específico utilizando SSE
-            public Flux<Notification> getNotificationsStream(String userId) {
-                return Flux.interval(Duration.ofMinutes(1))  // Emitir un evento cada minuto
-                        .flatMap(tick -> notificationRepository.findByUserReferenceId(userId)  // Buscar notificaciones por ID de usuario
-                                .doOnNext(notification -> System.out.println("Emitida notificación: " + notification.getMessage())))
-                        .repeat();  // Mantener el flujo activo
-            }
-
+    // Flujo de notificaciones para un usuario específico utilizando SSE
+    public Flux<Notification> getNotificationsStream(String userId) {
+        return Flux.interval(Duration.ofMinutes(1))  // Emitir un evento cada minuto
+                .flatMap(tick -> notificationRepository.findByUserReferenceId(userId)  // Buscar notificaciones por ID de usuario
+                        .doOnNext(notification -> System.out.println("Emitida notificación: " + notification.getMessage())))
+                .repeat();  // Mantener el flujo activo
+    }
 
 
-                    public Mono<String> markNotificationAsReadAndDelete(String notificationId) {
-                        // Encontrar la notificación por ID
-                        return notificationRepository.findById(notificationId)
-                                .flatMap(notification -> {
-                                    if (!notification.isRead()) {
-                                        // Marcar como leída
-                                        notification.setRead(true);
-                                        return notificationRepository.save(notification) // Guardar la notificación con el estado actualizado
-                                                .then(userRepository.findById(notification.getUserReferenceId()) // Encontrar al usuario dueño de la notificación
-                                                        .flatMap(user -> {
-                                                            // Remover la notificación de la lista de notificaciones del usuario
-                                                            user.getNotifications().remove(notification.getMessage());
-                                                            return userRepository.save(user) // Guardar los cambios en el usuario
-                                                                    .then(notificationRepository.deleteById(notificationId)) // Eliminar la notificación de la colección de notificaciones
-                                                                    .then(Mono.just("La notificación con ID: '" + notificationId + "' fue marcada como leída y eliminada con éxito."));
-                                                        })
-                                                );
-                                    } else {
-                                        // Ya está leída, solo la eliminamos
-                                        return userRepository.findById(notification.getUserReferenceId())
-                                                .flatMap(user -> {
-                                                    // Remover la notificación de la lista de notificaciones del usuario
-                                                    user.getNotifications().remove(notification.getMessage());
-                                                    return userRepository.save(user) // Guardar los cambios en el usuario
-                                                            .then(notificationRepository.deleteById(notificationId)) // Eliminar la notificación
-                                                            .then(Mono.just("La notificación con ID: '" + notificationId + "' ya estaba marcada como leída y ha sido eliminada."));
-                                                });
-                                    }
-                                })
-                                .switchIfEmpty(Mono.error(new RuntimeException("No se encontró la notificación con el ID proporcionado.")));
+    public Mono<String> markNotificationAsReadAndDelete(String notificationId) {
+        // Encontrar la notificación por ID
+        return notificationRepository.findById(notificationId)
+                .flatMap(notification -> {
+                    if (!notification.isRead()) {
+                        // Marcar como leída
+                        notification.setRead(true);
+                        return notificationRepository.save(notification) // Guardar la notificación con el estado actualizado
+                                .then(userRepository.findById(notification.getUserReferenceId()) // Encontrar al usuario dueño de la notificación
+                                        .flatMap(user -> {
+                                            // Remover la notificación de la lista de notificaciones del usuario
+                                            user.getNotifications().remove(notification.getMessage());
+                                            return userRepository.save(user) // Guardar los cambios en el usuario
+                                                    .then(notificationRepository.deleteById(notificationId)) // Eliminar la notificación de la colección de notificaciones
+                                                    .then(Mono.just("La notificación con ID: '" + notificationId + "' fue marcada como leída y eliminada con éxito."));
+                                        })
+                                );
+                    } else {
+                        // Ya está leída, solo la eliminamos
+                        return userRepository.findById(notification.getUserReferenceId())
+                                .flatMap(user -> {
+                                    // Remover la notificación de la lista de notificaciones del usuario
+                                    user.getNotifications().remove(notification.getMessage());
+                                    return userRepository.save(user) // Guardar los cambios en el usuario
+                                            .then(notificationRepository.deleteById(notificationId)) // Eliminar la notificación
+                                            .then(Mono.just("La notificación con ID: '" + notificationId + "' ya estaba marcada como leída y ha sido eliminada."));
+                                });
                     }
+                })
+                .switchIfEmpty(Mono.error(new RuntimeException("No se encontró la notificación con el ID proporcionado.")));
+    }
 
 
+    // Crear una nueva notificación para un usuario
+    public Mono<Notification> createNotification(String userId, Notification notification) {
+        if (notification.getId() == null || notification.getId().isEmpty()) {
+            notification.setId(UUID.randomUUID().toString().substring(0, 6));  // Generar un ID único
+        }
+        notification.setUserReferenceId(userId);  // Establecer la referencia del usuario
+        notification.setTimestamp(Instant.now());  // Establecer la marca de tiempo actual
+        notification.setRead(false);  // La notificación es nueva, por lo tanto no está leída
 
-                                        // Crear una nueva notificación para un usuario
-                                        public Mono<Notification> createNotification(String userId, Notification notification) {
-                                            if (notification.getId() == null || notification.getId().isEmpty()) {
-                                                notification.setId(UUID.randomUUID().toString().substring(0, 6));  // Generar un ID único
-                                            }
-                                            notification.setUserReferenceId(userId);  // Establecer la referencia del usuario
-                                            notification.setTimestamp(Instant.now());  // Establecer la marca de tiempo actual
-                                            notification.setRead(false);  // La notificación es nueva, por lo tanto no está leída
+        // Guardar la notificación
+        return notificationRepository.save(notification)
+                .flatMap(savedNotification -> {
+                    // Buscar el usuario por su ID
+                    return userRepository.findById(userId)
+                            .flatMap(user -> {
+                                // Agregar el ID de la nueva notificación a la lista de notificaciones del usuario
+                                user.getNotifications().add(savedNotification.getMessage());
+                                // Guardar el usuario actualizado
+                                return userRepository.save(user)
+                                        .thenReturn(savedNotification);  // Devolver la notificación guardada
+                            });
+                });
+    }
 
-                                            // Guardar la notificación
-                                            return notificationRepository.save(notification)
-                                                    .flatMap(savedNotification -> {
-                                                        // Buscar el usuario por su ID
-                                                        return userRepository.findById(userId)
-                                                                .flatMap(user -> {
-                                                                    // Agregar el ID de la nueva notificación a la lista de notificaciones del usuario
-                                                                    user.getNotifications().add(savedNotification.getMessage());
-                                                                    // Guardar el usuario actualizado
-                                                                    return userRepository.save(user)
-                                                                            .thenReturn(savedNotification);  // Devolver la notificación guardada
-                                                                });
-                                                    });
-                                        }
 
-//
+    /**
+     * @param message
+     * @return // Devuelve un Mono que contiene una respuesta http
+     * con un cuerpo de tipo CustomApiResponse
+     * que a su ves este contiene una lista de notificaciones.
+     */
+    // II.1 Metodo para busca los userReferenceId que contenga el mismo mensaje
+    public Mono<ResponseEntity<CustomApiResponse<List<Notification>>>> getNotificationsByMessage(String message) {
+        //1. Llama al metodo del repositorio para encontrar todas los usuarios  que tienen el mismo mensaje
+        return notificationRepository.findByMessage(message)
+                // 2. Recolectando todas las notificaciones en una lista
+                .collectList()
+                //3. Procesando las lista de notificaciones
+                .flatMap(notifications -> {
+                    // 4.1 Verifica si la lista de notificaciones está vacia
+                    if (notifications.isEmpty()) {
+                        return ResponseUtil.createErrorResponse("No se encontraron notificaciones para el mensaje", HttpStatus.NOT_FOUND);
+                    } else {//  4.2 Si la lista no está vacía regresa la lista
+                        return ResponseUtil.createSuccessResponse("Usuarios encontrados con el mismo mensaje", notifications);
+                    }
+                });
+        //.onErrorResume(e -> ResponseUtil.createErrorResponse("Error al obtener notificaciones para el mensaje", HttpStatus.INTERNAL_SERVER_ERROR));;
+    }
+
+
 }
